b5d347602e99867339ab1d35c2e71b5ebe917513
Владислав <possandglor@live.ru>
2024-06-03 21:05:32 +0300

    petlist updated

--- four-legs-front/src/components/pets/PetList.js
+++ four-legs-front/src/components/pets/PetList.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
-
+import './PetList.css'
 const PetList = () => {
   const [pets, setPets] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [selectedPet, setSelectedPet] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,12 +16,34 @@ const PetList = () => {
     fetchData();
   }, []);
 
+  const filteredPets = pets.filter(pet =>
+    pet.name.toLowerCase().includes(filter.toLowerCase()) 
+  );
+
   return (
-    <div>
-      <h1>Pets</h1>
+    <div className='pet-list'>
+      <div className="filter">
+        <input
+          type="text"
+          placeholder="Фильтр питомцев..."
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
       <ul>
-        {pets.map(pet => (
-          <li key={pet.petID}>{pet.name}</li>
+      {filteredPets.map(pet => (
+          <li key={pet.petID}>
+            <div className="pet-info">
+              <span>{pet.species} {pet.name} ({pet.breed})</span>
+              {/* <button onClick={() => handleSelectClient(pet)}>Edit</button>
+              <button onClick={() => handleDeleteClient(pet.clientID)}>Delete</button> */}
+            </div>
+            {/* <ul>
+              {pet.pets.map(pet => (
+                <li key={pet.petID}>{pet.name} ({pet.species})</li>
+              ))}
+            </ul> */}
+          </li>
         ))}
       </ul>
     </div>
